00f98e5b352af430c278b9fcfcc9b14573554f39
playmaker1210 <hatvanitamas09@gmail.com>
2022-12-13 11:15:11 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//fetch('http://localhost:3000/player').then(x => x.json()).then(y => megjelenit(y));
+fetch('http://localhost:3000/login').then(x => x.json()).then(y => megjelenit(y));
 
 function megjelenit(tomb){
     let sz = "";
 
     for (let elem of tomb) {
         sz += "<div>";
-        sz += elem.player_id + " " + elem.player_name + " " + elem.player_join_date;
+        sz += elem.username + " " + elem.password;
         sz += "</div>";
 
         document.getElementById('fetchData').innerHTML = sz;
