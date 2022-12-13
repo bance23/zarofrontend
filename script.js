e23c8d6a7b434a0f80ec1bbb827b86c7d4562d4c
playmaker1210 <hatvanitamas09@gmail.com>
2022-12-13 11:43:04 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 fetch('http://localhost:3000/login').then(x => x.json()).then(y => megjelenit(y));
+let userData;
 
 function megjelenit(tomb){
     let sz = "";
@@ -12,4 +13,23 @@ function megjelenit(tomb){
     }
 
     console.log(tomb);
+}
+
+function getDbData(data){
+    userData = data;
+}
+
+function login(){
+    let inputUsername = document.getElementById('username').value;
+    let passwordUsername = document.getElementById('password').value;
+
+    console.log(document.getElementById('username').value +  " " + document.getElementById('password').value);
+
+    for (let elem of userData) {
+        if(elem.username == inputUsername && elem.password == passwordUsername){
+            window.location.href = "fooldal.html";
+        }else{
+            alert("Szar vagy nem jo a loginod");
+        }
+    }
 }
